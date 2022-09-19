984000f37c32713ebb690f2c1d3d9a32cb769f77
WillyMayer1 <willianmayercl@gmail.com>
2022-09-19 12:55:07 -0300

    fixing

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,16 @@
 
 def main():
     
+    board = creating_board()
+
     print("Good game! Thank you for playing!")
+
+def creating_board():
+
+    print("1 | 2 | 3")
+    print("-+-+-+")
+    print("4 | 5 | 6")
+    print("-+-+-+")
+    print("7 | 8 | 9")
 
 main()
